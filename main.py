f436d283944e9b6a240bb84701e28972f7e38f1e
Aman Yadav <190050013@iitb.ac.in>
2022-10-14 21:01:25 +0530

    Add negative sample and modify edge training in GNN, testing todo

--- main.py
+++ main.py
@@ -4,19 +4,37 @@ from sklearn.metrics import roc_auc_score
 from message_passing import SAGEConv
 import torch
 from GNN import Net
+import random
 
 metaQAdata = MetaQADataset('/mnt/infonas/data/amanyadav/MetaQA')
 metaQAdata_train = metaQAdata.shuffle()
 # print(type(metaQAdata_train))
 # print(len(metaQAdata_train.x))
 
-batch_size= 1024
-train_loader = DataLoader(metaQAdata_train, batch_size=batch_size)
+# batch_size= 1024
+train_loader = DataLoader(metaQAdata_train)
 
 device = torch.device('cpu')
 model = Net(metaQAdata_train.x.size(dim=0), metaQAdata_train.edge_index[0].size(dim=0)).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 crit = torch.nn.BCELoss()
+
+def negative_sample(kg, answer_node_entity):
+    new_kg = []
+    all_answer_nodes = []
+    for elem in answer_node_entity:
+        for node in elem[0]:
+            all_answer_nodes.append(elem[0])
+    all_answer_nodes = all_answer_nodes.sort()
+    for node in kg.x:
+        found = False
+        for elem in all_answer_nodes:
+            if(node == elem):
+                found = True
+        if not found:
+            new_kg.append(node)
+    negative_sample = random.choice(new_kg)
+    return ([negative_sample], [], [])
 
 def train():
     model.train()
@@ -25,8 +43,7 @@ def train():
     for data in train_loader:
         data = data.to(device)
         optimizer.zero_grad()
-        output = model(data)
-        data.y = [0]
+        output, e = model(data)
         label = data.y.to(device)
         loss = crit(output, label)
         loss.backward()
